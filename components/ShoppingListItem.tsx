7a3c2ae95280891c74302d485e33460caf229bdd
Nikhil Shinde <nikhilshinde00@outlook.com>
2025-02-01 15:21:03 -0500

    styles, eslint

---
+++ components/ShoppingListItem.tsx
@@ -0,0 +1,93 @@
+import { StyleSheet, Text, View, Alert, TouchableOpacity } from "react-native";
+import { theme } from "../theme";
+
+type Props = {
+  name: string;
+  isCompleted?: boolean;
+};
+
+export function ShoppingListItem({ name, isCompleted }: Props) {
+  const handleDelete = () => {
+    Alert.alert(
+      `Are you sure you want to delete ${name}?`,
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok, deleting"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    );
+  };
+  return (
+    <View
+      style={[
+        styles.itemContainer,
+        isCompleted ? styles.completedContainer : undefined,
+      ]}
+    >
+      <Text
+        style={[
+          styles.itemText,
+          isCompleted ? styles.completedText : undefined,
+        ]}
+      >
+        {name}
+      </Text>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          isCompleted ? styles.completedButton : undefined,
+        ]}
+        onPress={handleDelete}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonColor}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    borderBottomColor: theme.colorCerulean,
+    borderBottomWidth: 1,
+    borderColor: "black",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorGrey,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  completedButton: {
+    backgroundColor: theme.colorGrey,
+  },
+  buttonColor: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
+});
